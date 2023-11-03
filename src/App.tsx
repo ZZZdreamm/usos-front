8cd421886c6e4ce797c1bcc1593beb9bfdb73230
ZZZdream <kmultan1234@gmail.com>
2023-11-03 21:58:13 +0100

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { AppLayout } from "./common/AppLayout/AppLayout";
 import Authorized from "./common/Authorized/Authorized";
+import { Footer } from "./components/Footer/Footer";
 import { PwBar } from "./components/PwBar/PwBar";
 import { TopBar } from "./components/TopBar/TopBar";
 import { Providers } from "./providers/Providers";
@@ -13,6 +14,7 @@ function App() {
         <Authorized isAuthorized={<TopBar />} />
         <Authorized isAuthorized={<PwBar />} />
         <RoutesProvider />
+        <Authorized isAuthorized={<Footer />} />
       </Providers>
     </AppLayout>
   );
