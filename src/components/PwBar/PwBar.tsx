84863ed1de87e91d6981142dc3d507617aa9406a
Jakub Kryczka <kryczka.jakub.pg2@gmail.com>
2023-11-03 16:45:19 +0100

    pwBar update

--- src/components/PwBar/PwBar.tsx
+++ src/components/PwBar/PwBar.tsx
@@ -9,6 +9,7 @@ export const PwBar = () => {
         <Image src="logoPW.png"/>
         <h2>Politechnika Warszawska</h2>
         <img src="buildingPic" alt="pwBuilding" />
+        <div className="lowerBar"></div>
       </div>
     </>
   );
