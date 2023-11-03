b02438b8a1840e5765bb2bd03e195768fe9c2f70
Jakub Kryczka <kryczka.jakub.pg2@gmail.com>
2023-11-03 21:51:02 +0100

    Navbar added

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { AppLayout } from "./common/AppLayout/AppLayout";
 import Authorized from "./common/Authorized/Authorized";
 import { PwBar } from "./components/PwBar/PwBar";
 import { TopBar } from "./components/TopBar/TopBar";
+import { NavBar } from "./components/NavBar/NavBar";
 import { Providers } from "./providers/Providers";
 import { RoutesProvider } from "./providers/RoutesProvider";
 
@@ -12,6 +13,7 @@ function App() {
       <Providers>
         <Authorized isAuthorized={<TopBar />} />
         <Authorized isAuthorized={<PwBar />} />
+        <Authorized isAuthorized={<NavBar />} />
         <RoutesProvider />
       </Providers>
     </AppLayout>
