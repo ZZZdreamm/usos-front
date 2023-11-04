261b1937763106331701bf9f5e6789f813a74648
ZZZdream <kmultan1234@gmail.com>
2023-11-04 01:41:56 +0100

    Add Redirect page and configure getting data from usos api

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const apiBaseUrl = "https://www.arcziweb.com/api/"
+// const apiBaseUrl = "https://www.arcziweb.com/api/"
+const apiBaseUrl = "http://localhost:8091/api/"
+
 
 export const axiosBase = axios.create({
     baseURL: apiBaseUrl
